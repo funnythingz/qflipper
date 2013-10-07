bf95870a4e9d1eaccbba740eb7a46ed5a2267ed8
funnythingz <hiroki@hackleberry.me>
2013-10-07 18:30:12 +0900

    zepto support

--- license/mit.js
+++ license/mit.js
@@ -1,6 +1,6 @@
 /**
 * qflipper.js
-* @version 1.0
+* @version 1.1
 * @author: Hiroki Oiwa;
 * @url: http://funnythingz.github.com/qflipper/
 * @license MIT (http://www.opensource.org/licenses/mit-license.php)
--- src/enum/transform-enum.ts
+++ src/enum/transform-enum.ts
@@ -1,10 +1,20 @@
 module Q {
 
+    // TODO: zeptoに対応するためにハイフン区切りにした
+    // ほんとはこうしたい
+    /*
     export enum TransformEnum {
         WebkitTransform,
         MozTransform,
         OTransform,
         msTransform
+    }*/
+
+    export enum TransformEnum {
+        '-webkit-transform',
+        '-moz-transform',
+        '-o-transform',
+        '-ms-transform'
     }
 
 }

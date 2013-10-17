30e0439d77c890f3aaa96b1c4de5037242fe8478
funnythingz <hiroki@hackleberry.me>
2013-10-17 18:49:10 +0900

    added qflipper.d.ts

---
+++ qflipper.d.ts
@@ -0,0 +1,29 @@
+/**
+* qflipper.d.ts
+* @version 1.2
+* @author: Hiroki Oiwa;
+* @url: http://funnythingz.github.com/qflipper/
+* @license MIT (http://www.opensource.org/licenses/mit-license.php)
+*/
+
+declare module Q {
+
+    export class Flipper {
+
+        constructor(
+                id: string,
+                args: any
+                );
+
+        refresh();
+        toNext();
+        toPrev();
+        moveToPoint(point: number);
+        hasNext(): boolean;
+        hasPrev(): boolean;
+        getPoint(): number;
+        getMaxPoint(): number;
+        flipElement(): JQuery;
+    }
+
+}

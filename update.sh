9b09cf035c7e1c850ca395f9f9fbcb730e7dc083
funnythingz <hiroki@hackleberry.me>
2014-07-15 16:26:26 +0900

    v1.4.1

--- update.sh
+++ update.sh
@@ -5,5 +5,6 @@ echo "[done] bower installed qflipper"
 rm -r js/qflipper.min.js
 echo "[done] rm js/qflipper.min.js"
 cp js/qflipper/qflipper.min.js js/
+rm -rf js/qflipper
 echo "[done] cp js/qflipper/qflipper.min.js js/qflipper.min.js"
 echo "updated qflipper.min.js"

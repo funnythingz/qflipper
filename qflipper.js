322d1851ee2feb0365f2cb8654f9f298bc54f8e6
funnythingz <funnythingz69+github@gmail.com>
2015-10-20 01:37:22 +0900

    v1.4.2

--- qflipper.js
+++ qflipper.js
@@ -1,6 +1,6 @@
 /**
 * qflipper.js
-* @version 1.4.1
+* @version 1.4.2
 * @author: Hiroki Oiwa;
 * @url: http://www.funnythingz.com/qflipper/
 * @license MIT (http://www.opensource.org/licenses/mit-license.php)
